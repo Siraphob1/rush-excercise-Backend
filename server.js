6c7c26c38a1edec8eddc5faa04bed0b6037d5f28
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-15 16:09:12 +0700

    add middlewares

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3500
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const helmet = require('helmet');
+const port = process.env.PORT || 3500;
+
+//3rd middlewares for Cross-origin resource sharing website
+app.use(cors());
+
+
+//built-in middlewares 
+//for handle urlencoded form data
+app.use(express.urlencoded({extended: false}));
+//for json
+app.use(express.json());
+//for hide x-powered
+app.disable('x-powered-by');
+
+
+//3rd middlewares for secure setting http response header
+app.use(helmet());
+
+//custom middlewares
+
 
 
 app.get('/' , (req,res)=>{
-    res.send('Welcome to API RUSH excercise ')
+    res.json('Welcome to API RUSH excercise ')
 })
 
 
