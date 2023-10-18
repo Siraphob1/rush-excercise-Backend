32a4050cd159b07a28441935cdaf9ba0d1f140db
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-19 00:02:27 +0700

    add verifyController to route

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const signupController = require('../controllers/signupController');
+const signupController = require('../controllers/signup/signupController');   
+const verifyController = require('../controllers/signup/verifyController');
+
 const router = express.Router();
 
-router.post('/', signupController)
+router.post('/', signupController);
+router.get('/verify', verifyController);
 
 module.exports = router
