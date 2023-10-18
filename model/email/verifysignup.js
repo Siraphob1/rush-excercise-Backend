c2b244642de8bd7f9950ac87a8b1977f62dbb221
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-19 00:01:25 +0700

    create html email verify

---
+++ model/email/verifysignup.js
@@ -0,0 +1,119 @@
+const messageVerifyEmail = (username , urlVerify) =>{
+    return(`
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+  <head>
+    <!-- Compiled with Bootstrap Email version: 1.4.0 --><meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no, date=no, address=no, email=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <style type="text/css">
+      body,table,td{font-family:Helvetica,Arial,sans-serif !important}.ExternalClass{width:100%}.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div{line-height:150%}a{text-decoration:none}*{color:inherit}a[x-apple-data-detectors],u+#body a,#MessageViewBody a{color:inherit;text-decoration:none;font-size:inherit;font-family:inherit;font-weight:inherit;line-height:inherit}img{-ms-interpolation-mode:bicubic}table:not([class^=s-]){font-family:Helvetica,Arial,sans-serif;mso-table-lspace:0pt;mso-table-rspace:0pt;border-spacing:0px;border-collapse:collapse}table:not([class^=s-]) td{border-spacing:0px;border-collapse:collapse}@media screen and (max-width: 600px){.max-w-150,.max-w-150>tbody>tr>td{max-width:600px !important;width:100% !important}.w-full,.w-full>tbody>tr>td{width:100% !important}.h-9,.h-9>tbody>tr>td{height:36px !important}.h-112,.h-112>tbody>tr>td{height:448px !important}.pt-2:not(table),.pt-2:not(.btn)>tbody>tr>td,.pt-2.btn td a,.py-2:not(table),.py-2:not(.btn)>tbody>tr>td,.py-2.btn td a{padding-top:8px !important}.pb-2:not(table),.pb-2:not(.btn)>tbody>tr>td,.pb-2.btn td a,.py-2:not(table),.py-2:not(.btn)>tbody>tr>td,.py-2.btn td a{padding-bottom:8px !important}.p-4:not(table),.p-4:not(.btn)>tbody>tr>td,.p-4.btn td a{padding:16px !important}.pr-4:not(table),.pr-4:not(.btn)>tbody>tr>td,.pr-4.btn td a,.px-4:not(table),.px-4:not(.btn)>tbody>tr>td,.px-4.btn td a{padding-right:16px !important}.pl-4:not(table),.pl-4:not(.btn)>tbody>tr>td,.pl-4.btn td a,.px-4:not(table),.px-4:not(.btn)>tbody>tr>td,.px-4.btn td a{padding-left:16px !important}*[class*=s-lg-]>tbody>tr>td{font-size:0 !important;line-height:0 !important;height:0 !important}.s-2>tbody>tr>td{font-size:8px !important;line-height:8px !important;height:8px !important}.s-9>tbody>tr>td{font-size:36px !important;line-height:36px !important;height:36px !important}}
+    </style>
+  </head>
+  <body class="bg-gray-300 " style="outline: 0; width: 100%; min-width: 100%; height: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: Helvetica, Arial, sans-serif; line-height: 24px; font-weight: normal; font-size: 16px; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; color: #000000; margin: 0; padding: 0; border-width: 0;" bgcolor="#e2e8f0">
+    <table class="bg-gray-300 body" valign="top" role="presentation" border="0" cellpadding="0" cellspacing="0" style="outline: 0; width: 100%; min-width: 100%; height: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: Helvetica, Arial, sans-serif; line-height: 24px; font-weight: normal; font-size: 16px; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; color: #000000; margin: 0; padding: 0; border-width: 0;" bgcolor="#e2e8f0">
+      <tbody>
+        <tr>
+          <td valign="top" style="line-height: 24px; font-size: 16px; margin: 0;" align="left" bgcolor="#e2e8f0">
+            <table class="max-w-150 h-112 bg-white ax-center ay-top p-4 w-full" role="presentation" align="center" border="0" cellpadding="0" cellspacing="0" style="max-width: 600px; width: 100%; height: 448px; vertical-align: top !important; margin: 0 auto;" bgcolor="#ffffff" width="100%" height="448">
+              <tbody>
+                <tr>
+                  <td style="line-height: 24px; font-size: 16px; max-width: 600px; width: 100%; height: 448px; margin: 0; padding: 16px;" align="left" bgcolor="#ffffff" valign="top" width="100%" height="448">
+                    <img src="https://media.discordapp.net/attachments/1138852917294661682/1164228799148339340/4.png?ex=65427390&amp;is=652ffe90&amp;hm=d64718cf2891ebc28e5134565dd8d65813d9614246940e98c5cd9d8f1c19c0b6&amp;=&amp;width=1016&amp;height=468" class="h-9" alt="" style="height: 36px; line-height: 100%; outline: none; text-decoration: none; display: block; border-style: none; border-width: 0;" height="36">
+                    <table class="s-9 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                      <tbody>
+                        <tr>
+                          <td style="line-height: 36px; font-size: 36px; width: 100%; height: 36px; margin: 0;" align="left" width="100%" height="36">
+                            &#160;
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <table class="pl-4" role="presentation" border="0" cellpadding="0" cellspacing="0">
+                      <tbody>
+                        <tr>
+                          <td style="line-height: 24px; font-size: 16px; padding-left: 16px; margin: 0;" align="left">
+                            <h3 class="" style="padding-top: 0; padding-bottom: 0; font-weight: 500; vertical-align: baseline; font-size: 28px; line-height: 33.6px; margin: 0;" align="left">Hello , ${username}</h3>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <table class="s-9 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                      <tbody>
+                        <tr>
+                          <td style="line-height: 36px; font-size: 36px; width: 100%; height: 36px; margin: 0;" align="left" width="100%" height="36">
+                            &#160;
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <p class="text-center fw-700" style="line-height: 24px; font-size: 16px; font-weight: 700 !important; width: 100%; margin: 0;" align="center">Thanks for signing up.</p>
+                    <table class="s-2 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                      <tbody>
+                        <tr>
+                          <td style="line-height: 8px; font-size: 8px; width: 100%; height: 8px; margin: 0;" align="left" width="100%" height="8">
+                            &#160;
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <p class="text-center" style="line-height: 24px; font-size: 16px; width: 100%; margin: 0;" align="center">Please confirm your email address to get full access to RUSH</p>
+                    <table class="s-9 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                      <tbody>
+                        <tr>
+                          <td style="line-height: 36px; font-size: 36px; width: 100%; height: 36px; margin: 0;" align="left" width="100%" height="36">
+                            &#160;
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <p class="text-center fw-700" style="line-height: 24px; font-size: 16px; font-weight: 700 !important; width: 100%; margin: 0;" align="center">&#3586;&#3629;&#3610;&#3588;&#3640;&#3603;&#3607;&#3637;&#3656;&#3621;&#3591;&#3607;&#3632;&#3648;&#3610;&#3637;&#3618;&#3609;</p>
+                    <table class="s-2 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                      <tbody>
+                        <tr>
+                          <td style="line-height: 8px; font-size: 8px; width: 100%; height: 8px; margin: 0;" align="left" width="100%" height="8">
+                            &#160;
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <p class="text-center" style="line-height: 24px; font-size: 16px; width: 100%; margin: 0;" align="center">&#3585;&#3619;&#3640;&#3603;&#3634;&#3618;&#3639;&#3609;&#3618;&#3633;&#3609;&#3585;&#3634;&#3619;&#3621;&#3591;&#3607;&#3632;&#3648;&#3610;&#3637;&#3618;&#3609;&#3629;&#3637;&#3648;&#3617;&#3621;&#3621;&#3660;&#3648;&#3614;&#3639;&#3656;&#3629;&#3607;&#3637;&#3656;&#3607;&#3656;&#3634;&#3609;&#3592;&#3632;&#3626;&#3634;&#3617;&#3634;&#3619;&#3606;&#3648;&#3586;&#3657;&#3634;&#3606;&#3638;&#3591; RUSH &#3652;&#3604;&#3657;</p>
+                    <table class="s-9 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                      <tbody>
+                        <tr>
+                          <td style="line-height: 36px; font-size: 36px; width: 100%; height: 36px; margin: 0;" align="left" width="100%" height="36">
+                            &#160;
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <table class="ax-center" role="presentation" align="center" border="0" cellpadding="0" cellspacing="0" style="margin: 0 auto;">
+                      <tbody>
+                        <tr>
+                          <td style="line-height: 24px; font-size: 16px; margin: 0;" align="left">
+                            <a href=${urlVerify} class="bg-black text-yellow-500  px-4 py-2 rounded-md  underline" style="color: #ffc107; background-color: #000000; border-radius: 6px; text-decoration: underline; padding: 8px 16px;">Confirm your account here</a>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
+
+
+    
+    
+    `)
+};
+
+
+module.exports = messageVerifyEmail;
