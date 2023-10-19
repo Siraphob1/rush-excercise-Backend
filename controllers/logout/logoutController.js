68d606378c61e7f141f4f5c4ea1ea6f80310932a
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-19 22:56:48 +0700

    check cookie findRFT and deleteRFT

--- controllers/logout/logoutController.js
+++ controllers/logout/logoutController.js
@@ -1,6 +1,27 @@
-const logoutController = (req ,res) =>{
-    const {userid} = req.params
-    res.json({"message":userid})
+const userModel = require('../../model/userSchema');
+
+const logoutController = async (req ,res) =>{
+    const cookies = req.cookies;
+
+    //check has cookies JWT
+    if(!cookies?.jwt) return res.sendStatus(204);
+    const refreshToken = cookies.jwt;
+
+    //is refreshToken in DB
+    const queryrefreshToken = {refreshToken , refreshToken};
+    const userFind = await userModel.findOne(queryrefreshToken);
+    if(!userFind){
+        res.clearCookie('jwt', {httpOnly: true});
+        return res.sendStatus(204);
+    }
+
+    //delete refreshToken in DB
+    const userUpdate = {refreshToken: ''}
+    const resultUpdate = await userModel.findOneAndUpdate(queryrefreshToken , userUpdate);
+
+    res.clearCookie('jwt', {httpOnly: true});
+    res.sendStatus(204);
+   
 }
 
 module.exports = logoutController;
