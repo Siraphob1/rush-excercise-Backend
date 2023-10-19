99bbdde77ffe8b19dc0bd4d233d5fb13e0cfbd8b
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-19 17:27:41 +0700

    add route refresh

--- server.js
+++ server.js
@@ -16,14 +16,12 @@ connectDBCompass();
 
 //3rd middlewares for Cross-origin resource sharing website
 app.use(cors(corsOptions));
-
 //built-in middlewares for handle urlencoded form data
 app.use(express.urlencoded({extended: false}));
 //built-in middlewares for json
 app.use(express.json());
 //built-in middlewares for hide x-powered
 app.disable('x-powered-by');
-
 //3rd middlewares for secure setting http response header
 app.use(helmet());
 
@@ -31,16 +29,18 @@ app.use(helmet());
 
 
 
-//routes
+//routes public
 app.get('/' , (req,res)=>{
     res.json('Welcome to API RUSH excercise ')
 })
 app.use('/signup', require('./routes/signup.js'));
 app.use('/login', require('./routes/login.js'));
-
+app.use('/refresh' , require('./routes/refresh.js'));
 
 //custom middlewares
 app.use(verifyJWT)
+
+//routes private
 app.use('/api/activity' , require('./routes/api/activity.js'))
 
 mongoose.connection.once('open', ()=>{
