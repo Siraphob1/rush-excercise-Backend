32f7b81b300d31136a89b784e5a02ef46a385abd
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-21 19:39:07 +0700

    create route profile

---
+++ routes/profile.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const readProfileController = require('../controllers/profile/readprofileController');
+const updateProfileController = require('../controllers/profile/updateProfileController');
+
+const router = express.Router();
+
+
+router.get('/:userID', readProfileController);
+router.put('/:userID', updateProfileController);
+
+module.exports = router
