aab67e0df76b331fa132b21b2926f57893db7307
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-22 16:54:15 +0700

    update status

--- controllers/activity/updateActivityController.js
+++ controllers/activity/updateActivityController.js
@@ -11,13 +11,14 @@ const updateActivityController = async (req ,res) =>{
     if(!activityID) return res.status(400).json({"error":"bad request"});
 
     //name or description was not attached in req body
-    const {name , description} = req.body;
+    const {name , description , status} = req.body;
     if(!name || !description) return res.status(400).json({"error":"bad request"});
-    
+        
     
     try {
         //find activity and update in DB
-        const updateData = {name:name , description:description};
+        const updateData = status ? {name:name , description:description ,status: status}
+                                  : {name:name , description:description}
         const activityFind = await activityModel.findByIdAndUpdate(activityID , updateData);
         if(!activityFind) return res.sendStatus(404);
         // console.log(activityFind)
