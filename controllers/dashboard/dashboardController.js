a8604308e0cf73c1839e16e5ebc411b2efb3b850
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-22 18:07:16 +0700

    add find group of activities and count

--- controllers/dashboard/dashboardController.js
+++ controllers/dashboard/dashboardController.js
@@ -1,7 +1,36 @@
-const dashboardController = (req ,res) =>{
-    const {userid} = req.query
-    console.log(req.query);
-    res.status(200).json({"message":userid})
+const activityModel = require('../../model/activitySchema');
+
+const dashboardController = async (req ,res) =>{
+
+    //userID was not attached in req body
+    const {userID} = req.params
+    if(!userID) return res.status(400).json({"error":"bad request"});
+
+   try {
+         //find group of activities  and  count
+        const aggregate = await activityModel.aggregate([
+            {
+                $group:{
+                    _id: "$status",
+                    count: {$count:{}}
+                }
+            }
+        ])
+
+        //format data before response
+        const aggregateResponse = aggregate.map((element)=>{
+            const activity = {
+                status: element._id,
+                count:element.count
+            }
+            return activity;
+        })    
+        res.status(200).json({"message":aggregateResponse})
+    
+   } catch (error) {
+        //server error
+        res.sendStatus(500)
+   }
 }
 
 module.exports = dashboardController;
