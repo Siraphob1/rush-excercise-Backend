4c7b024a9e705d2c0131c8325fe9d265764a1caf
Siraphob1 <Sirapop_t@hotmail.com>
2023-10-22 18:08:46 +0700

    comment

--- routes/notfound.js
+++ routes/notfound.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.all('/' , (req , res)=>{
-    res.sendStatus(404);
+router.all('/*' , (req , res)=>{
+    //not found this route
+    res.sendStatus(404);    
 })
 
 module.exports = router;
